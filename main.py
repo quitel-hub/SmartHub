5872beaa4eddd58e1ca8a84843274fa6d2d9185b
quitel-hub <karpenko.k.a.07@gmail.com>
2026-05-03 14:51:55 +0300

    add new requirement and extra settings to main.py

--- main.py
+++ main.py
@@ -2,33 +2,37 @@ import asyncio
 import os
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
+from aiohttp import web
 
 load_dotenv() 
 
 from bot.handlers import router
 from services.database import DatabaseAdapter
 
+async def handle(request):
+    return web.Response(text="SmartHub Bot is Running!")
+
 async def main():
-    print("🚀 Starting SmartHub...")
+    
+    app = web.Application()
+    app.router.add_get("/", handle)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    
+    port = int(os.getenv("PORT", 10000)) 
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
+    print(f"🌐 Web server started on port {port}")
+    
     db = DatabaseAdapter()
-    
     bot_token = os.getenv("BOT_TOKEN")
-    if not bot_token:
-        print("❌ Помилка: BOT_TOKEN не знайдено в .env")
-        return
-    
     bot = Bot(token=bot_token)
     dp = Dispatcher()
-    
     dp.include_router(router)
     
-    print("🤖 Bot is successfully started and waiting for messages!")
-
+    print("🚀 Starting SmartHub Bot...")
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
     
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        print("\n🛑 SmartHub stopped.")
+    asyncio.run(main())
